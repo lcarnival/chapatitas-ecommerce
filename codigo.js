1853f8809c04acf1b9b832eb43976ba3efc659f2
Lucila Carnival <lucilacarnival@MacBook-Air-de-Lucila.local>
2023-09-06 12:07:11 -0300

    se agregaron array y metodos de orden superior

--- codigo.js
+++ codigo.js
@@ -1,48 +1,7 @@
 /* El proyecto tratara de un ecommerce sobre la comercializacion de chapitas para mascotas */
 
-let nombre = prompt('Ingresa tu nombre');
 
-let saludo = 'Hola ' + nombre;
-alert(saludo);
-
-
-alert('Aqui encontraras las opciones de chapitas para que elijas la que mas te guste:')
-printMenu('hola putita');
-
-let opcionChapatita = prompt('Ingrese opcion de chapita o 0 para salir');
-
-while (opcionChapatita != '0') {
-
-    switch (opcionChapatita) {
-        case '1':
-            alert('Chapita Mickey 3500');
-            break;
-        case '2':
-            alert('chapita Minnie 3500');
-            break;
-        case '3':
-            alert('chapita capital america 4000');
-            break;
-        case '4':
-            alert('chapita capital Mora 3000');
-            break;
-        case '5':
-            alert('chapita Stich 5000');
-            break;
-        case '6':
-            alert('chapita Donut 3500');
-        default:
-            alert('La opcion no existe');
-            break;
-                    
-    }
-
-    opcionChapatita = prompt('Ingrese opcion de chapita o 0 para salir');
-
-}
-
-function printMenu(miTextito) {
-    console.log(miTextito);
+function printMenu() {
     console.log('chapita Mickey presiona la opcion 1');
     console.log('chapita Minnie presiona la opcion 2');
     console.log('chapita Capian America presiona la opcion 3');
@@ -51,7 +10,9 @@ function printMenu(miTextito) {
     console.log('chapita Donut presiona la opcion 6');
 }
 
-function startMenu(selectedOption) {
+function startMenu() {
+    let selectedOption = prompt('Ingrese opcion de chapita o 0 para salir');
+
     while (selectedOption != '0') {
 
         switch (selectedOption) {
@@ -62,7 +23,7 @@ function startMenu(selectedOption) {
                 alert('chapita Minnie 3500');
                 break;
             case '3':
-                alert('chapita capital america 4000');
+                alert('chapita Arnold 4000');
                 break;
             case '4':
                 alert('chapita capital Mora 3000');
@@ -72,6 +33,7 @@ function startMenu(selectedOption) {
                 break;
             case '6':
                 alert('chapita Donut 3500');
+                break;
             default:
                 alert('La opcion no existe');
                 break;
@@ -83,24 +45,99 @@ function startMenu(selectedOption) {
     }
 }
 
-let envio = prompt ('desea envio a domicilio? 1-SI 2-NO')
-let necesitaEnvio;
+function shippingType (ordenCompra) {
+    let envio = ''
 
-if (envio == '1') {
-    necesitaEnvio = true;
-} else if (envio == '2') {
-    necesitaEnvio = false;
-}else {
-    alert ('error');
+    do {
+
+        envio = prompt ('desea envio a domicilio? 1-SI 2-NO')
+    
+        if (envio == '1') {
+            ordenCompra.envioADomicilio = true;
+            ordenCompra.direccionUsuario = prompt ('Ingrese su direccion'); 
+            
+        } else if (envio == '2') {
+            ordenCompra.envioADomicilio = false;
+        }  else {
+            alert ('La opcion es inexistente');
+        }
+    
+        } while (envio != '1' && envio != '2');
+        
+        console.log(ordenCompra);
+        
+}
+
+class OrdenDeCompra {
+    constructor (nombreDeUsuario){
+        this.nombreDeUsuario = nombreDeUsuario;
+        this.envioADomicilio = false;
+        this.direccionUsuario = '';
+        this.tipoDePago = '';
+        this.carritoDeCompra = [];
+    }
 }
 
 
 
+let nombre = prompt('Ingresa tu nombre');
+
+const orden = new OrdenDeCompra(nombre);
+
+let saludo = 'Hola ' + nombre;
+alert(saludo);
+
+alert('Aqui encontraras las opciones de chapitas para que elijas la que mas te guste:')
+
+printMenu();
+
+startMenu();
+
+shippingType(orden);
+
+/*
+const baratos = producto.filter (producto => productos.precio < 3000);
+console.log (baratos);
+
+*/
+
+/*
+const existe = productos.some (productos => productos.tipo === 'minnie');
+console.log (existe);
+*/
+
+function filtrarPorPrecioMax (precioMaximo) {
+const filtrados = productos.filter ((producto)=> producto.Precio < precioMaximo);
+console.log (filtrados);
+
+}
+
+filtrarPorPrecioMax (3400);
 
 
 
-/* class Perro {
-    constructor(nombre, raza, edad, color, tama) {
 
+
+    console.log ('En 10 dias habiles le llegara su pedido al domicilio indicado, gracias por su compra')
+
+
+    /*
+
+    class Chapatita {
+    constructor (modelo, tamaño, precio) {
+        this.modelo = modelo;
+        this.tamaño = tamaño;
+        this.precio = precio;
     }
-} */
+}
+
+const chapita1 = new Chapatita ('Minnie','M','3500');
+const chapita2 = new Chapatita ('Mickey','L', '3500');
+const chapita3 = new Chapatita ('Arnold','S','4000');
+const chapita4 = new Chapatita ('Mora','XL','3000');
+const chapita5 = new Chapatita ('Stich','XL','5000');
+const chapita6 = new Chapatita ('Donut','S','2500');
+
+*/
+
+console.log (productos)
---
+++ productos.js
@@ -0,0 +1,51 @@
+const productos = [{
+    Tipo: 'Mickey', 
+    Talle: ['m','l','xl'],
+    Precio: 4000,
+
+},
+
+{ 
+    Tipo: 'Minnie', 
+    Talle: ['xs','l','xl'],
+    Precio: 3500,
+
+},
+
+{
+    Tipo: 'Arnold', 
+    Talle: ['s','m','xxl'],
+    Precio: 4000,
+
+},
+
+{
+    Tipo: 'Mora', 
+    Talle: ['xs','s','xl'],
+    Precio: 2900,
+
+},
+
+{
+    Tipo: 'Stich', 
+    Talle: ['xs','s','m','l'],
+    Precio: 5200,
+
+},
+
+{
+    Tipo: 'Donut', 
+    Talle: ['xs','s','m','l','xl'],
+    Precio: 4900,
+
+},
+
+
+];
+
+console.table (productos);
+
+
+
+
+
